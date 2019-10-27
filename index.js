7e009f4d7baa0c43f7e96ac7d863b07e32228b13
Sam Yang <samabc75@gmail.com>
2019-10-28 00:58:55 +0800

    Add lot of message response

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bot = new SlackBot({
 
 // Start Handler
 bot.on('start', () => {
-    bot.postMessageToChannel('chatroom', '喵喵喵~ 我是SIRLA好幫喵，上線喵喵喵');
+    bot.postMessageToUser('samabc75', '喵喵喵~ 我是SIRLA好幫喵，上線喵喵喵，會叫的喵是好喵');
 });
 
 // Error Handler
@@ -19,15 +19,37 @@ bot.on('message', (data) => {
     if (data.type !== 'message') {
         return;
     }
-    handleMessage(data.text, data.channel);
+    handleMessage(data);
 });
 
 // Response to message
-function handleMessage(message, channel_id) {
-    if (message.includes('喵喵講話')) {
+let words = ['喵喵喵喵喵喵喵貓喵喵喵喵喵喵', '櫻櫻沒袋子，沒事叫我講幹話做啥', '你有聽過無限貓咪理論嗎，貓咪有一天也能打出曠世巨作', '幹話。', '喵喵累了，喵喵不講幹話', '喵', '喵喵喵', '喵喵~喵喵喵~~咪邀~~~'];
+
+function handleMessage(data) {
+    if (data.text.includes('喵喵講話')) {
         bot.getChannels().then( (value) => {
-            let channel_name = value.channels.find(items => items.id === channel_id).name;
+            let channel_name = value.channels.find(items => items.id === data.channel).name;
             bot.postMessageToChannel(channel_name, '話。');
+        });
+    } else if (data.text.includes('喵喵講幹話')) {
+        bot.getChannels().then( (value) => {
+            let channel_name = value.channels.find(items => items.id === data.channel).name;
+            let response = words[Math.floor(Math.random() * words.length)];
+            bot.postMessageToChannel(channel_name, response);
+        });
+    } else if (data.text.includes('喵喵誰最帥')) {
+        bot.getChannels().then( (value) => {
+            let channel_name = value.channels.find(items => items.id === data.channel).name;
+            bot.getUserById(data.user).then( value => {
+                bot.postMessageToChannel(channel_name, `喵~ ${value.profile.display_name}最帥了!`);
+            });
+        });
+    } else if (data.text.includes('喵喵誰最美')) {
+        bot.getChannels().then( (value) => {
+            let channel_name = value.channels.find(items => items.id === data.channel).name;
+            bot.getUserById(data.user).then( value => {
+                bot.postMessageToChannel(channel_name, `喵~ ${value.profile.display_name}最美了!`);
+            });
         });
     }
 }
