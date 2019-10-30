b15797cff8b8d44902f29f20db4de830db4fdd8d
Sam Yang <samabc75@gmail.com>
2019-10-30 16:42:56 +0800

    Fix get users list

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ slackEvents.on('message', (event) => {
     })();
   } else if (event.text.includes("喵喵誰最帥")){
     (async () => {
-      let user_name = users.list.members.find(items => items.id === event.user).name;
+      let user_name = web.users.list.members.find(items => items.id === event.user).name;
       const result = await web.chat.postMessage({
         text: `喵~${user_name}最帥了`,
         channel: event.channel,
@@ -42,7 +42,7 @@ slackEvents.on('message', (event) => {
     })();
   } else if (event.text.includes("喵喵誰最美")){
     (async () => {
-      let user_name = users.list.members.find(items => items.id === event.user).name;
+      let user_name = web.users.list.members.find(items => items.id === event.user).name;
       const result = await web.chat.postMessage({
         text: `喵~${user_name}最美了`,
         channel: event.channel,
