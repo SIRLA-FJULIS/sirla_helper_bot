4931f13cd5ff9a47604812eca1f274c127650d82
Sam Yang <samabc75@gmail.com>
2019-10-30 13:46:45 +0800

    Initial again

---
+++ index.js
@@ -0,0 +1,23 @@
+const { createEventAdapter } = require('@slack/events-api');
+
+// Read the signing secret from the environment variables
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+
+// Initialize
+const slackEvents = createEventAdapter(slackSigningSecret);
+
+// Read the port from the environment variables, fallback to 3000 default.
+const port = process.env.PORT || 3000;
+
+// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+slackEvents.on('message', (event) => {
+    console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+});
+
+(async () => {
+  // Start the built-in server
+  const server = await slackEvents.start(port);
+
+  // Log a message when the server is ready
+  console.log(`Listening for events on ${server.address().port}`);
+})();
