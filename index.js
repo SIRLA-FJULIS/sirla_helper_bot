9c2379ff0763274677ff99283a2ab21ba45c77d3
Sam Yang <samabc75@gmail.com>
2019-10-30 16:59:26 +0800

    Fix user display name finding

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ slackEvents.on('message', (event) => {
     })();
   } else if (event.text.includes("喵喵誰最帥")){
     web.users.list().then( (value) => {
-      let user_name = value.members.find(item => items.id === event.user).profile.display_name;
+      let user_name = value.members.find(item => item.id === event.user).profile.display_name;
       (async () => {
         const result = await web.chat.postMessage({
           text: `喵~${user_name}最帥了`,
@@ -44,7 +44,7 @@ slackEvents.on('message', (event) => {
     });
   } else if (event.text.includes("喵喵誰最美")){
     web.users.list().then( (value) => {
-      let user_name = value.members.find(item => items.id === event.user).profile.display_name;
+      let user_name = value.members.find(item => item.id === event.user).profile.display_name;
       (async () => {
         const result = await web.chat.postMessage({
           text: `喵~${user_name}最美了`,
